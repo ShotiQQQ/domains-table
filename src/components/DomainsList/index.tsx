060266fa15dcf7170da3cc744f25981aed876f29
denis.gordievsky <denis.gordievsky@1win.pro>
2024-08-12 19:13:00 +0700

    add search input(not ended)

--- src/components/DomainsList/index.tsx
+++ src/components/DomainsList/index.tsx
@@ -2,19 +2,44 @@ import styles from './DomainsList.module.scss';
 import DomainsListHeader from './DomainsListHeader';
 import DomainsListItem from './DomainsListItem';
 import { domainsList } from 'models/domainsList';
-import { useState } from 'react';
+import { ChangeEvent, useState } from 'react';
 import { TDomainList } from 'types';
+import Input from '../UI/Input';
 
 const DomainsList = () => {
   const [currentDomainsList, setCurrentDomainsList] =
     useState<TDomainList>(domainsList);
+  const [filteredDomainsList, setFilteredDomainsList] = useState<TDomainList>([...currentDomainsList]);
+  const [searchInputValue, setSearchInputValue] = useState('');
+
+  const setCurrentSearchInputValue = (event: ChangeEvent<HTMLInputElement>) => {
+    setSearchInputValue(event.target.value);
+
+    setTimeout(() => {
+      searchItems(event.target.value);
+    }, 300);
+  }
+
+  const searchItems = (value: string) => {
+    const includedItems = currentDomainsList.filter((item) => item.domainName.includes(value));
+
+    if (includedItems.length && searchInputValue) {
+      setFilteredDomainsList([...includedItems])
+    } else {
+      setFilteredDomainsList([...currentDomainsList])
+    }
+  }
 
   return (
     <div className={styles.listContainer}>
+      <div className={styles.listSearch}>
+        <Input value={searchInputValue} placeholder='Поиск' onChange={setCurrentSearchInputValue} />
+      </div>
+
       <DomainsListHeader />
 
       <ul className={styles.list}>
-        {currentDomainsList.map(({ domainName, isAvailable }) => {
+        {filteredDomainsList.map(({ domainName, isAvailable }) => {
           return (
             <DomainsListItem
               domainName={domainName}
